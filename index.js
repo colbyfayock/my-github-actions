4afc588762a7a13f4b883238d76252dc8b147452
Colby Fayock <fayock@gmail.com>
2020-05-30 23:29:38 -0400

    Step 3: intentionally breaking code to see tests fail

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function mooncakeSays(message) {
-  let mooncake = '(o.o)';
+  let mooncake = '(O.o)';
 
-  if ( !message ) {
-    return `${mooncake} chookity?`;
-  }
+  if ( !message ) mooncake;
 
   return `${mooncake} ${message}`;
 }
